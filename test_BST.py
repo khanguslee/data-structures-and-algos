a7d854d8c48485098e8b794b0411b4413ff31c21
khanguslee <khanguslee@gmail.com>
2019-12-15 23:08:31 +1100

    Create unit tests for contains method

--- test_BST.py
+++ test_BST.py
@@ -9,6 +9,38 @@ class TestBST:
         test_bst = BST(test_value)
         assert test_bst.root.value == test_value
 
+    def test_contains_root_value(self):
+        test_value = 50
+        test_bst = BST(test_value)
+        assert (test_value in test_bst) == True
+
+    def test_contains_value(self):
+        # Generate test BST
+        test_value = 50
+        find_value = 30
+        test_bst = BST(test_value)
+        test_bst.insert(60)
+        test_bst.insert(10)
+        test_bst.insert(20)
+        test_bst.insert(find_value)
+        test_bst.insert(5)
+
+        assert (find_value in test_bst) == True
+
+    def test_does_not_contain_value(self):
+        # Generate test BST
+        test_value = 50
+        find_value = 100
+        test_bst = BST(test_value)
+        test_bst.insert(60)
+        test_bst.insert(10)
+        test_bst.insert(20)
+        test_bst.insert(30)
+        test_bst.insert(5)
+
+        assert (find_value in test_bst) == False
+
+
     def test_print_inorder(self, capfd):
         # Generate test BST
         test_value = 50
