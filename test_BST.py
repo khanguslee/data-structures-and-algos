90570277eef1578a9855b5c7c191c3cc05b2cc73
khanguslee <khanguslee@gmail.com>
2019-12-15 22:33:10 +1100

    Add ability to test inorder printing of BST

--- test_BST.py
+++ test_BST.py
@@ -7,3 +7,15 @@ class TestBST:
         test_value = 50
         test_bst = BST(test_value)
         assert test_bst.root.value == test_value
+
+    def test_print_inorder(self, capfd):
+        test_value = 50
+        test_bst = BST(test_value)
+        test_bst.insert(40)
+        test_bst.insert(60)
+        test_bst.print_inorder()
+        out, _ = capfd.readouterr()
+        actual_result = out.strip()
+        expected_result = str([40, 50, 60])
+        assert actual_result == expected_result
+
