1dc44caab5f2c347f340e67bb0918e14651968fd
khanguslee <khanguslee@gmail.com>
2019-12-15 21:56:10 +1100

    Add ability to print BST inorder

--- BST.py
+++ BST.py
@@ -9,18 +9,19 @@ class BST:
         self.root = Node(root_value)
 
     def insert(self, input_value):
-        self.insert_recurse(self.root, input_value)
+        self.root = self.insert_recurse(self.root, input_value)
 
     def insert_recurse(self, input_node, input_value):
         if input_node is None:
-            return Node(input_value)
+            input_node = Node(input_value)
         else:
             if input_value < input_node.value:
-                self.insert_recurse(input_node.left, input_value)
+                input_node.left = self.insert_recurse(input_node.left, input_value)
             elif input_value > input_node.value:
-                self.insert_recurse(input_node.right, input_value)
+                input_node.right = self.insert_recurse(input_node.right, input_value)
             else:
                 raise ValueError("Duplicate Value")
+        return input_node
 
     def search(self, input_value):
         # TODO: Search for a value
@@ -29,9 +30,21 @@ class BST:
     def delete(self, input_value):
         # TODO: Delete value from tree
         pass
+    
+    def print_inorder(self):
+        output_array = []
+        output_array = self.print_inorder_recurse(self.root, output_array)
+        print(output_array)
+    
+    def print_inorder_recurse(self, input_node, input_array):
+        if input_node is not None:
+            input_array = self.print_inorder_recurse(input_node.left, input_array)
+            input_array.append(input_node.value)
+            input_array = self.print_inorder_recurse(input_node.right, input_array)
+        return input_array
 
 if __name__ == "__main__":
      binary_search_tree = BST(50)
      binary_search_tree.insert(70)
      binary_search_tree.insert(10)
-     print(binary_search_tree)
+     binary_search_tree.print_inorder()
